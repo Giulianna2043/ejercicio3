3182606e6571c042aeb11f049d70a19e6c66a641
giulianna <j.bustinza42@gmail.com>
2016-12-19 17:37:36 -0500

    ejercicio3

---
+++ main.js
@@ -0,0 +1,8 @@
+var horas, pago, sueldoSemanal;
+
+horas = prompt("Por favor, ingresa un número");
+pago = prompt("Por favor, ingresa otro número");
+
+sueldoSemanal= parseInt(horas) * parseInt(pago)
+
+alert(sueldoSemanal);
